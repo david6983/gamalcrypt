e14ee0eb767b0cb0ca1ba2411680b2c4ab02f6af
david haioum <dav6983@gmail.com>
2020-04-03 14:49:08 +0800

    question2

--- src/edu/upm/Main.java
+++ src/edu/upm/Main.java
@@ -1,8 +1,31 @@
 package edu.upm;
 
+import edu.upm.security.cryptography.Cryptorithm;
+
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String plaintext = JOptionPane.showInputDialog(new JFrame(),
+                "Please enter a message for encryption",
+                "Input",
+                JOptionPane.QUESTION_MESSAGE);
+
+        System.out.println(Cryptorithm.encrypt(plaintext));
+        JOptionPane.showMessageDialog(new JFrame(),
+                "The ciphertext for the message (Hexadecimal Form): \n" + Cryptorithm.encrypt(plaintext),
+                "Output",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        String ciphertext = JOptionPane.showInputDialog(new JFrame(),
+                "Please enter a ciphertext for decryption",
+                "Input",
+                JOptionPane.QUESTION_MESSAGE);
+
+        JOptionPane.showMessageDialog(new JFrame(),
+                "The original message for the ciphertext is : " + Cryptorithm.decrypt(ciphertext),
+                "Output",
+                JOptionPane.INFORMATION_MESSAGE);
     }
 }
